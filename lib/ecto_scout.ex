a4cd2efd4f4193649d745f8f60321d45a1aba57c
Patrick Veverka <pveverka@wombatsecurity.com>
2017-04-17 18:57:14 -0400

    first commit

---
+++ lib/ecto_scout.ex
@@ -0,0 +1,18 @@
+defmodule EctoScout do
+  @moduledoc """
+  Documentation for EctoScout.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoScout.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
